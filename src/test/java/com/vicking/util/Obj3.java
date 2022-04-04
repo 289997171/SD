fc111e70be9711fc3981b522bf4ccb390eaca19b
eclipser1 <5379326+eclipser1@user.noreply.gitee.com>
2022-04-05 00:19:55 +0800

    忽略属性

--- src/test/java/com/vicking/util/Obj3.java
+++ src/test/java/com/vicking/util/Obj3.java
@@ -1,5 +1,6 @@
 package com.vicking.util;
 
+import com.vicking.util.ann.SD_NO;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,36 +10,40 @@ import java.util.*;
 @Getter @Setter @ToString
 public class Obj3 {
     private int iv;
+
+    @SD_NO
     private Integer iv2;
 
+    @SD_NO
     public int iv3;
+
     public Integer iv4;
 
-    private ArrayList<Boolean> bs1;
-    public ArrayList<Boolean> bs2;
-
-    private HashSet<Boolean> bs3;
-    public HashSet<Boolean> bs4;
-
-    private boolean[] bs5;
-    public boolean[] bs6;
-
-    private Obj4 obj4;
-
-    private ArrayList<Obj4> obj41;
-    public ArrayList<Obj4> obj42;
-
-    private HashSet<Obj4> obj43;
-    public HashSet<Obj4> obj44;
-
-    private Obj4[] obj45;
-    public Obj4[] obj46;
-
-    private HashMap<Integer, String> map1;
-    private HashMap<Integer, ArrayList<String>> map2;
-    private HashMap<Integer, HashSet<String>> map3;
-    private HashMap<Integer, String[]> map4;
-    private HashMap<Integer, boolean[]> map5;
+//    private ArrayList<Boolean> bs1;
+//    public ArrayList<Boolean> bs2;
+//
+//    private HashSet<Boolean> bs3;
+//    public HashSet<Boolean> bs4;
+//
+//    private boolean[] bs5;
+//    public boolean[] bs6;
+//
+//    private Obj4 obj4;
+//
+//    private ArrayList<Obj4> obj41;
+//    public ArrayList<Obj4> obj42;
+//
+//    private HashSet<Obj4> obj43;
+//    public HashSet<Obj4> obj44;
+//
+//    private Obj4[] obj45;
+//    public Obj4[] obj46;
+//
+//    private HashMap<Integer, String> map1;
+//    private HashMap<Integer, ArrayList<String>> map2;
+//    private HashMap<Integer, HashSet<String>> map3;
+//    private HashMap<Integer, String[]> map4;
+//    private HashMap<Integer, boolean[]> map5;
 
     public HashMap<Integer, HashMap<Integer, HashMap<Integer, Float>>> map6;
 
--- src/test/java/com/vicking/util/Test5.java
+++ src/test/java/com/vicking/util/Test5.java
@@ -22,65 +22,65 @@ public class Test5 {
 //        SDBuilder.build(Obj4.class);
     }
 
-    @Test
-    void test2() throws IOException {
-        long begin = System.currentTimeMillis();
-        Obj3 o = new Obj3();
-        o.setIv(1);
-        o.setIv2(2);
-        o.setIv3(3);
-        o.setIv4(4);
-        o.setBs1(Lists.newArrayList(true, false, true, false));
-        o.setBs2(Lists.newArrayList(true, false, true, false));
-        o.setBs3(Sets.newHashSet(true, false, true, false));
-        o.setBs4(Sets.newHashSet(true, false, true, false));
-        o.setBs5(new boolean[]{true, false, true, false});
-        o.setBs6(new boolean[]{true, false, true, false});
-        Obj4 obj4 = new Obj4();
-        obj4.setId(123);
-        obj4.setName("my name is xxx");
-        o.setObj4(obj4);
-        o.setObj41(Lists.newArrayList(obj4,obj4,obj4));
-        o.setObj42(Lists.newArrayList(obj4,obj4,obj4));
-        o.setObj43(Sets.newHashSet(obj4,obj4,obj4));
-        o.setObj44(Sets.newHashSet(obj4,obj4,obj4));
-        o.setObj45(new Obj4[]{obj4,obj4,obj4});
-        o.setObj46(new Obj4[]{obj4,obj4,obj4});
-//        o.setMap1(new HashMap<Integer,String>());
-//        o.setMap2(new HashMap<Integer,String>>());
-//        o.setMap3(new HashMap<Integer,String>>());
-//        o.setMap4(new HashMap<Integer,String[]>());
-//        o.setMap5(new HashMap<Integer,boolean[]>());
-//        o.setMap6(new HashMap<Integer,Integer,Integer,Float>>>());
-
-        HashMap<Integer, Float> map = new HashMap<Integer, Float>();
-        map.put(1, 1f);
-        map.put(2, 2f);
-        map.put(3, 3f);
-        map.put(4, 4f);
-        map.put(5, 5f);
-
-        HashMap<Integer, HashMap<Integer, Float>> map1 = new HashMap<>();
-        map1.put(1, map);
-        map1.put(2, map);
-        map1.put(3, map);
-        map1.put(4, map);
-
-        o.setMap6(new HashMap<>());
-
-        o.getMap6().put(1, map1);
-        o.getMap6().put(2, map1);
-        o.getMap6().put(3, map1);
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Obj3Writer.write(os, o);
-        System.out.println(os.size());
-
-        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
-        Obj3 read = Obj3Reader.read(is);
-        System.out.println(o);
-        System.out.println(read);
-
-        System.out.println("Cost " + (System.currentTimeMillis() - begin));
-    }
+//    @Test
+//    void test2() throws IOException {
+//        long begin = System.currentTimeMillis();
+//        Obj3 o = new Obj3();
+//        o.setIv(1);
+//        o.setIv2(2);
+//        o.setIv3(3);
+//        o.setIv4(4);
+//        o.setBs1(Lists.newArrayList(true, false, true, false));
+//        o.setBs2(Lists.newArrayList(true, false, true, false));
+//        o.setBs3(Sets.newHashSet(true, false, true, false));
+//        o.setBs4(Sets.newHashSet(true, false, true, false));
+//        o.setBs5(new boolean[]{true, false, true, false});
+//        o.setBs6(new boolean[]{true, false, true, false});
+//        Obj4 obj4 = new Obj4();
+//        obj4.setId(123);
+//        obj4.setName("my name is xxx");
+//        o.setObj4(obj4);
+//        o.setObj41(Lists.newArrayList(obj4,obj4,obj4));
+//        o.setObj42(Lists.newArrayList(obj4,obj4,obj4));
+//        o.setObj43(Sets.newHashSet(obj4,obj4,obj4));
+//        o.setObj44(Sets.newHashSet(obj4,obj4,obj4));
+//        o.setObj45(new Obj4[]{obj4,obj4,obj4});
+//        o.setObj46(new Obj4[]{obj4,obj4,obj4});
+////        o.setMap1(new HashMap<Integer,String>());
+////        o.setMap2(new HashMap<Integer,String>>());
+////        o.setMap3(new HashMap<Integer,String>>());
+////        o.setMap4(new HashMap<Integer,String[]>());
+////        o.setMap5(new HashMap<Integer,boolean[]>());
+////        o.setMap6(new HashMap<Integer,Integer,Integer,Float>>>());
+//
+//        HashMap<Integer, Float> map = new HashMap<Integer, Float>();
+//        map.put(1, 1f);
+//        map.put(2, 2f);
+//        map.put(3, 3f);
+//        map.put(4, 4f);
+//        map.put(5, 5f);
+//
+//        HashMap<Integer, HashMap<Integer, Float>> map1 = new HashMap<>();
+//        map1.put(1, map);
+//        map1.put(2, map);
+//        map1.put(3, map);
+//        map1.put(4, map);
+//
+//        o.setMap6(new HashMap<>());
+//
+//        o.getMap6().put(1, map1);
+//        o.getMap6().put(2, map1);
+//        o.getMap6().put(3, map1);
+//
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//        Obj3Writer.write(os, o);
+//        System.out.println(os.size());
+//
+//        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
+//        Obj3 read = Obj3Reader.read(is);
+//        System.out.println(o);
+//        System.out.println(read);
+//
+//        System.out.println("Cost " + (System.currentTimeMillis() - begin));
+//    }
 }
